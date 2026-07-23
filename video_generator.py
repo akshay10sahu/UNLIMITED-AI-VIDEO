caf08c0f4bc18c06b28f0beef6c95ccf2fa57b83
akshay10sahu <rishisahu1999r@gmail.com>
2026-07-23 10:37:56 +0530

    Update video_generator.py

--- video_generator.py
+++ video_generator.py
@@ -1,1 +1,9 @@
 print("Video Generator Ready")
+import os
+
+HF_TOKEN = os.getenv("HUGGINGFACE_API_TOKEN")
+
+def check_token():
+    if not HF_TOKEN:
+        return "Hugging Face Token नहीं मिला।"
+    return "Token Connected Successfully"
